9708028ddc2ad483b1974dcfa8558cf3315f1d8c
Godstime Asosa <aisosawise360@gmail.com>
2025-09-24 19:53:30 +0100

    modified commit

--- backend/Server.js
+++ backend/Server.js
@@ -1,1 +1,22 @@
-console.log('Server Running...')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT  || 5000
+
+const app = express()
+
+app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`))
+
+
+// app.get('/', (req, res)=>{
+//     res.json({message: 'Welcome to Support Desk API'})
+// })
+
+
+app.get('/', (req, res)=>{
+    res.status(201).json({message: 'Welcome to Support Desk API'})
+})
+
+app.post('/', (req, res)=>{
+    res.status(200).json({message: 'new user created'})
+})
+
