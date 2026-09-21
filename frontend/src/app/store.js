7dac4a447935e6178b8b796b3aa5ad734c7d5c78
Godstime Asosa <aisosawise360@gmail.com>
2026-09-21 11:20:45 +0100

    Fix auth slice import

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from '../features/auth/authslice';
+import authReducer from '../features/auth/authSlice';
 import ticketReducer from '../features/ticket/ticketSlice'
 import noteReducer from '../features/note/noteSlice'
 
